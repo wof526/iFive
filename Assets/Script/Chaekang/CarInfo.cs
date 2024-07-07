ef50dadf0d3cce6784b19e1b31265347080f992c
chaekang <herry612@naver.com>
2024-07-08 02:47:15 +0900

    feat/유저가 선택한 차종 연동 및 대쉬 보완
    * 유저가 선택한 차종의 이름, 설명을 유저 정보와 함께 파이어베이스로 전송
    * 대쉬 기능 보완

---
+++ Assets/Script/Chaekang/CarInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarInfo : MonoBehaviour
+{
+    public float maxHp;
+    public float maxSpeed;
+    public float dashSpeed;
+    public float zeroBaek;
+}
