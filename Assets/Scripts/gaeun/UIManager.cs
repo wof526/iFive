d31ae752ede39d3d53d8f711206239cb8dde7796
yonokuso <qltenwkd@naver.com>
2024-07-10 22:25:24 +0900

    Commit UI Setting
    VFX Resetting, UI modifying

--- Assets/Scripts/gaeun/UIManager.cs
+++ Assets/Scripts/gaeun/UIManager.cs
@@ -1,31 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.UI;
 using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
+    Vector2 targetPos;
+    private RectTransform rectTransform;
+    bool IsGageActive = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
         DOTween.Init();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rectTransform = GetComponent<RectTransform>();
+        targetPos = rectTransform.anchoredPosition;
+        //Debug.Log(targetPos);
     }
 
     public void BtnMoveFoward()
     {
-        transform.DOMoveX(630, 0.2f, false); //targetPosition
+        rectTransform.DOAnchorPosX(targetPos.x - 70f, 0.2f, false); //targetPosition
     }
 
     public void BtnMoveBack()
     {
-        transform.DOMoveX(726, 0.2f, false); //targetPosition
+        rectTransform.DOAnchorPosX(targetPos.x, 0.2f, false); //targetPosition
     }
 
-
+    public void Gagebar()
+    {
+        // if tag or objectname = gagebar -> setactivefalse;
+        // 하나씩 setactive true
+        // max값 자료에서 보기
+    }
 }
