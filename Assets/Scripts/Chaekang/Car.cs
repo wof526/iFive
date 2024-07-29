02ee78f780fd9272d108f6096a504a3523cafb53
ally432 <ally432@naver.com>
2024-07-29 15:32:49 +0900

    Revert "Merge pull request #19 from ally432/chaekang"
    This reverts commit 820bd1e73c7f31d2c874c98f59648933156f81a2, reversing
    changes made to 6dfc772e015f90fa990f153a29369bdc437804da.

--- Assets/Scripts/Chaekang/Car.cs
+++ Assets/Scripts/Chaekang/Car.cs
@@ -6,13 +6,10 @@ using Firebase.Auth;
 
 public class Car : MonoBehaviour
 {
-    private Slider HPBar;
-    private TMP_Text HPText;
-    private GameObject gameOverPanel;
-    private TMP_Text gameOverTxt;
-    private Button Skill;
-    private Button Break;
-    private GameObject joyStick;
+    [SerializeField] private Slider HPBar;
+    [SerializeField] private TMP_Text HPText;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private TMP_Text gameOverTxt;
 
     public GameObject fire;   // 불방구 용
     public GameObject smoke;  // 게임 오버 용
@@ -33,6 +30,9 @@ public class Car : MonoBehaviour
     Drive drive;
     Dash dash;
 
+    [SerializeField] private Button Skill;
+    [SerializeField] private Button Break;
+
     private FirebaseAuth auth;
     private Rigidbody rb;
 
@@ -46,57 +46,12 @@ public class Car : MonoBehaviour
         drive = GameManager.Instance.drive;
         dash = GameManager.Instance.dash;
 
-        // Find and assign UI components
-        HPBar = FindInActiveObjectByName("HPBar")?.GetComponent<Slider>();
-        if (HPBar != null) Debug.Log("HPBar found and assigned.");
-        else Debug.LogError("HPBar not found!");
-
-        HPText = FindInActiveObjectByName("HPText")?.GetComponent<TMP_Text>();
-        if (HPText != null) Debug.Log("HPText found and assigned.");
-        else Debug.LogError("HPText not found!");
-
-        gameOverPanel = FindInActiveObjectByName("GameOverPanel");
-        if (gameOverPanel != null)
-        {
-            Debug.Log("gameOverPanel found and assigned.");
-            gameOverTxt = gameOverPanel.transform.Find("GameOverTxt")?.GetComponent<TMP_Text>();
-            if (gameOverTxt != null) Debug.Log("gameOverTxt found and assigned.");
-            else Debug.LogError("gameOverTxt not found!");
-        }
-        else Debug.LogError("gameOverPanel not found!");
-
-        Skill = FindInActiveObjectByName("SkillBtn")?.GetComponent<Button>();
-        if (Skill != null) Debug.Log("Skill found and assigned.");
-        else Debug.LogError("Skill not found!");
-
-        Break = FindInActiveObjectByName("BreakBtn")?.GetComponent<Button>();
-        if (Break != null) Debug.Log("Break found and assigned.");
-        else Debug.LogError("Break not found!");
-
-        joyStick = FindInActiveObjectByName("Joystick");
-        if (joyStick != null) Debug.Log("Joystick found and assigned.");
-        else Debug.LogError("Joystick not found!");
-
         // HP Management
         maxHP = GameManager.Instance.CarInfo.maxHp;
         curHP = maxHP;
         SetMaxHealth(maxHP);
 
         maxSpeed = GameManager.Instance.CarInfo.maxSpeed;
-    }
-
-    // Method to find inactive GameObject by name
-    private GameObject FindInActiveObjectByName(string name)
-    {
-        Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
-        for (int i = 0; i < objs.Length; i++)
-        {
-            if (objs[i].hideFlags == HideFlags.None && objs[i].name == name)
-            {
-                return objs[i].gameObject;
-            }
-        }
-        return null;
     }
 
     // HP Initial Setting
@@ -203,10 +158,11 @@ public class Car : MonoBehaviour
 
     private IEnumerator RestartCountdown()
     {
-        Skill.gameObject.SetActive(false);
-        Break.gameObject.SetActive(false);
-        dash.dashButton.gameObject.SetActive(false);
-        joyStick.gameObject.SetActive(false);
+        Skill.interactable = false;
+        Break.interactable = false;
+        dash.dashButton.interactable = false;
+        Joystick.isFree = false;
+
 
         while (countdown > 0)
         {
@@ -255,10 +211,10 @@ public class Car : MonoBehaviour
             Debug.LogError("User is not logged in.");
         }
 
-        Skill.gameObject.SetActive(true);
-        Break.gameObject.SetActive(true);
-        dash.dashButton.gameObject.SetActive(true);
-        joyStick.gameObject.SetActive(true);
+        Skill.interactable = true;
+        Break.interactable = true;
+        dash.dashButton.interactable = true;
+        Joystick.isFree = true;
 
         yield return null;
     }
