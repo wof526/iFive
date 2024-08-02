cffb9cfaca4cabcd3977fc703f25ccc77f7a145e
yonokuso <qltenwkd@naver.com>
2024-08-02 16:18:49 +0900

    Commit 점령지_3
    팀에 따른 점령지 색 변경(2)

--- Assets/Scripts/gaeun/FXtriggerManager.cs
+++ Assets/Scripts/gaeun/FXtriggerManager.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class FXtriggerManager : MonoBehaviour
 {
     public MapFXManager mapFXManager;
+    public float areaSecBlue = 60.0f;
+    public float areaSecRed = 60.0f;
+
+    private int RedCollisionCount = 0;
+    private bool isPaused = false;
 
 
     private void OnTriggerStay(Collider collider)
     {
+        string coltag = collider.gameObject.tag;
+
 
         switch (collider.gameObject.tag)
         {
@@ -16,12 +23,17 @@ public class FXtriggerManager : MonoBehaviour
                 switch (collider.gameObject.tag)
                 {
                     case "Team Red":
+                        isPaused = true;
+
                         mapFXManager.FXchangerYellow();
-                        Debug.Log("case Blue");
+                        Debug.Log("case Blue-red");
                         break;
 
                     default:
+                        isPaused = true;
+
                         mapFXManager.FXchangerBlue();
+                        CountSecBlue();
                         break;
                 }
                 break;
@@ -31,20 +43,58 @@ public class FXtriggerManager : MonoBehaviour
                 switch (collider.gameObject.tag)
                 {
                     case "Team Blue":
+                        //isPaused = true;
+
                         mapFXManager.FXchangerYellow();
-                        Debug.Log("case Blue");
+                        Debug.Log("case Red-blue");
                         break;
 
                     default:
+                        //isPaused = true;
+
                         mapFXManager.FXchangerRed();
+                        CountSecRed();
                         break;
                 }
                 break;
 
             default:
-                //mapFXManager.FXchangerYellow();
-                Debug.Log("case Yellow");
+                if (isPaused) // pause yellow
+                {
+                    StartCoroutine(PauseCoroutineBlue(coltag));
+                    Debug.Log("pause yellow");
+                    //isPaused = false; // restart yellowFX
+
+                }
+
+                else
+                {
+                    mapFXManager.FXchangerYellow();
+                    Debug.Log("case Yellow");
+                }
                 break;       
         }
     }
+
+
+    public void CountSecBlue()
+    {
+        areaSecBlue -= Time.deltaTime;
+    }
+
+    public void CountSecRed()
+    {
+        areaSecRed -= Time.deltaTime;
+    }
+
+    //--------------------------------------------------------
+
+            
+
+    private IEnumerator PauseCoroutineBlue(string coltag) // Coroutine : pause yellow
+    {   Debug.Log("start coroutine");
+
+        yield return new WaitUntil(() => coltag == "Team Blue"); // pause until all blue
+    }
+
 }
